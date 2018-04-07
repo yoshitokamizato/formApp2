db1ce1a4de84ecaedadd0612323df84554dd344f
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-04-07 18:49:03 +0900

    add user search

--- formApp2/app/controllers/user_controller.rb
+++ formApp2/app/controllers/user_controller.rb
@@ -26,4 +26,28 @@ class UserController < ApplicationController
     user = User.find(params[:id])
     user.destroy
   end
+
+
+  def searchId
+    id1 = params[:id1]
+    id2 = params[:id2]
+    user = User.new
+
+    if id2.nil? || id2.empty?
+      #render :text => "値を入力してください"
+      if  id1.nil? || id1.empty?
+        flash[:notice] = "入力してください"
+        redirect_to action: :search
+      end
+
+      return @user = user.searchAll
+    end
+
+    @user = user.searchId(id1, id2)
+  end
+
+  def searchData
+    user = User.new
+    @user = user.searchData(params[:user])
+  end
 end
--- formApp2/app/models/user.rb
+++ formApp2/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  def searchAll
+    return User.all
+  end
+
+  def searchId(id1,id2)
+    return User.find_by_sql(['select * from users where id >= :id1 and id <= :id2', {id1: id1, id2: id2}])
+  end
+
+  def searchData(user)
+    return User.find_by_sql(['SELECT * FROM users WHERE CONCAT(id, name, address, gender, program, skils) LIKE :user', {user: "%#{user}%"}])
+  end
 end
